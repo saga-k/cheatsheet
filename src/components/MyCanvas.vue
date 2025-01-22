dd07a410e4027e1981df8a6a2693b1716959128e
saga-k <sagakrusell+1@gmail.com>
2025-01-22 15:17:28 +0100

    canvas resizing works

---
+++ src/components/MyCanvas.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  name: 'MyCanvas',
+
+  data(){
+    return{
+      height:null,
+      width: null
+    }
+  },
+
+  props:{
+    sizeProp:{
+      type: Object
+    }
+  }
+}
+
+</script>
+
+<template>
+
+<section>
+</section>
+
+</template>
+
+<style scoped>
+section{
+  border: 1px solid black;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,29 @@
 <script>
+import MyCanvas from '@/components/MyCanvas.vue';
 import SizeInput from '@/components/SizeInput.vue';
 
 export default{
+
 name: 'HomeView',
+
 components:{
   SizeInput,
+  MyCanvas
 },
+
+data(){
+return{
+  canvasSize:{
+    height: null,
+    width: null
+  }
+}
+},
+
 methods:{
   onSetSize(Size){
-  console.log('size', Size)
+  this.canvasSize.height = `${Size.height}px`;
+  this.canvasSize.width = `${Size.width}px`;
   }
 }
 }
@@ -17,4 +32,5 @@ methods:{
 <template>
 <h1>test</h1>
 <SizeInput @SetSize="onSetSize"/>
+<MyCanvas :style="{height:canvasSize.height, width: canvasSize.width}"/>
 </template>
