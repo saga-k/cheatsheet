621730753f4a6ab68eb35b740a3dc76aed32ce12
saga-k <sagakrusell+1@gmail.com>
2025-01-22 14:08:20 +0100

    clean commit

---
+++ src/components/SizeInput.vue
@@ -0,0 +1,9 @@
+<script>
+export default{
+  name:'SizeInput'
+}
+</script>
+
+<template>
+<input type="text">
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script>
+import SizeInput from '@/components/SizeInput.vue';
+
+export default{
+name: 'HomeView',
+components:{
+  SizeInput,
+}
+}
+</script>
+
+<template>
+<h1>test</h1>
+<SizeInput/>
+</template>
