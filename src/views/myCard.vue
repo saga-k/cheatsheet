3067d6c605e8c12974068145428de507ddd34085
saga-k <sagakrusell+1@gmail.com>
2025-01-29 16:46:59 +0100

    commit before testing dragable and breaking things

--- src/views/myCard.vue
+++ src/views/myCard.vue
@@ -1,55 +1,57 @@
 <script>
 export default {
-  name: 'myCard',
+  name: "myCard",
 
-  props:{
-    cardProp:{
+  props: {
+    cardProp: {
       type: Object,
-      required: true
-    }
+      required: true,
+    },
   },
 
-  data(){
+  data() {
     return {
       cardTitle: this.cardProp.title,
       code: this.cardProp.code,
-      description: this.cardProp.description
-    }
-  }
-
-}
+      description: this.cardProp.description,
+    };
+  },
+};
 </script>
 
 <template>
   <section>
-    <h3>{{cardTitle}}</h3>
-    <p v-if="description!==null">{{ description }}</p>
+    <h3>{{ cardTitle }}</h3>
+    <p v-if="description !== null">{{ description }}</p>
     <div>
-    <pre><code>{{ code }}</code></pre>
+      <pre><code>{{ code }}</code></pre>
     </div>
-
   </section>
 </template>
 
 <style scoped>
-
-section{
-  border: solid black 1px;
+section {
+  background-color: #eff3f6;
+  border: solid #c9c9c9 1px;
   padding: 0.5rem;
   height: fit-content;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  gap: 0.2rem;
 }
 
-p{
+p {
   font-size: 14px;
 }
-code{
+code {
   font-size: 14px;
 }
 
-div{
-  background-color: lightgray;
+div {
+  background-color: white;
   padding: 0.5rem;
+  border-radius: 5px;
+  border: solid 1px #c9c9c9;
 }
-
-
 </style>
