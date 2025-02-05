a2ddc945e0a17e8c2d4a8036b058e72222811a59
saga-k <sagakrusell+1@gmail.com>
2025-02-05 16:18:12 +0000

    started on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,36 @@
 <script>
 </script>
 <template>
-  <h1>Testing router</h1>
+  <article id="fullLayout">
+
+    <section id="heroCopy">
+      <h1>About the app</h1>
+      <p class="pLarge">
+        Create the perfect custom JavaScript cheat sheet with ease! This app lets you design and personalize cheat
+        sheets for quick reference or sharing. Whether you’re a beginner or a pro, you can build a cheat sheet that’s
+        tailored to your needs.
+      </p>
+    </section>
+
+    <section id="features">
+
+    </section>
+
+  </article>
 </template>
+
+<style scoped>
+#fullLayout {
+  max-width: 1000px;
+  margin: auto;
+}
+
+#heroCopy {
+  margin-top: 10rem;
+  margin-bottom: 10rem;
+}
+
+#heroCopy p {
+  max-width: 700px;
+}
+</style>
